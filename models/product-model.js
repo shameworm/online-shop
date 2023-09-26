0c803fad17502fb81da9a7410317a73e1629cfc4
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2023-09-26 12:48:11 +0300

    Fetching product items

--- models/product-model.js
+++ models/product-model.js
@@ -8,7 +8,23 @@ class Product {
         this.description = productData.description;
         this.image = productData.image; // Name of the product image file
         this.imagePath = `product-data/images/${productData.image}`; // image path
-        this.imageUrl = `/products/assets/images/${productData.image}`; 
+        this.imageUrl = `/products/assets/images/${productData.image}`;
+
+        if (productData._id) {
+            this.id = productData._id.toString();
+        }
+    }
+
+    static async findAll() {
+        const products = await db
+            .getDatabase()
+            .collection("products")
+            .find()
+            .toArray();
+
+        return products.map((productDoc) => {
+            return new Product(productDoc);
+        });
     }
 
     async save() {
@@ -17,7 +33,7 @@ class Product {
             summary: this.summary,
             price: this.price,
             description: this.description,
-            image: this.image
+            image: this.image,
         };
 
         await db.getDatabase().collection("products").insertOne(productDoc);
