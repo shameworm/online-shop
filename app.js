4cfe292c6019b036b68626769f15c354b27f306c
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2023-09-28 18:57:33 +0300

    Cart basic functionality

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const errorHandlerMiddleware = require("./middlewares/error-handler");
 const checkAuthStatusMiddleware = require("./middlewares/check-authentication-status");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const routesProtectMiddleware = require("./middlewares/protect-routes");
+const cartMiddleware = require("./middlewares/cart");
 
 const authRouter = require("./routes/auth-routes");
 const productsRouter = require("./routes/products-routes");
 const baseRouter = require("./routes/base-routes");
 const adminRouter = require("./routes/admin-routes");
+const cartRouter = require("./routes/cart-routes");
 
 const app = express();
 
@@ -25,11 +27,14 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use("/products/assets", express.static("product-data"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
+
+app.use(cartMiddleware);
 
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
@@ -37,6 +42,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRouter);
 app.use(authRouter);
 app.use(productsRouter);
+app.use("/cart", cartRouter);
 app.use(routesProtectMiddleware);
 app.use("/admin", adminRouter);
 
---
+++ public/scripts/cart-management.js
@@ -0,0 +1,36 @@
+const addToCartButtonElement = document.querySelector("#product-details button");
+const cartBangeElement = document.querySelector(".nav-items .badge");
+
+addToCartButtonElement.addEventListener("click", async () => {
+    const productId = addToCartButtonElement.dataset.productid;
+    const csrfToken = addToCartButtonElement.dataset.csrf;
+    
+    let response;
+    try {
+
+        response = await fetch("/cart/items", {
+            method: "POST",
+            body: JSON.stringify({
+                productId: productId,
+                _csrf: csrfToken,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error) {
+        alert("Something went wrong");
+        return;
+    }
+
+    if (!response.ok) {
+        alert("Something went wrong!");
+        return;
+    }
+
+    const responseData = await response.json();
+
+    const newTotalQuantity = responseData.newTotalItems;
+
+    cartBangeElement.textContent = newTotalQuantity;
+});
